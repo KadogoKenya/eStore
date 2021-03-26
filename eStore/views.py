27b1631221acfccdde111c90a90ff57d7af37595
Kate <you@example.com>
2021-03-26 12:52:57 +0300

    initial commit

---
+++ eStore/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+def store(request):
+
+    context{}
+
+    return render(request, 'store/store.htmls',context)
+
+
+def cart(request):
+    
+    context{}
+
+    return render(request, 'store/cart.htmls',context)
+
+
+def checkout(request):
+    
+    context{}
+
+    return render(request, 'store/checkout.htmls',context)
+
+
+def main(request):
+    
+    context{}
+
+    return render(request, 'store/main.htmls',context)
+
